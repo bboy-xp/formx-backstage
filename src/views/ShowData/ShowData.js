5dc01ddc704bc01170127ae76a55a100b0519129
unknown <1169470657@qq.com>
2018-11-15 20:24:47 +0800

    11.15

--- src/views/ShowData/ShowData.js
+++ src/views/ShowData/ShowData.js
@@ -12,11 +12,19 @@ import heartImg from '../../assets/img/heart.png';
 import githubImg from '../../assets/img/github.png';
 import headImg from '../../assets/img/head.jpg';
 
-import { Button, Menu, Breadcrumb } from 'element-react';
+import { Button, Menu, Breadcrumb, DatePicker, } from 'element-react';
 
 //引入包装好的逻辑
 import util from '../../lib/util';
 const { checkLogin, gotoHome, gotoShowData } = util;
+
+// // 引入 ECharts 主模块
+// import echarts from 'echarts/dist/echarts.common';
+// // 引入折线图
+// import  'echarts/lib/chart/line';
+// // 引入提示框和标题组件
+// import 'echarts/lib/component/tooltip';
+// import 'echarts/lib/component/title';
 
 export default class Home extends Component {
 
@@ -24,6 +32,69 @@ export default class Home extends Component {
     super(props);
     this.state = {
       date: new Date(),
+      selectDate: new Date(),
+      option: {
+        title: {
+          text: '折线图堆叠'
+        },
+        tooltip: {
+          trigger: 'axis'
+        },
+        legend: {
+          data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        toolbox: {
+          feature: {
+            saveAsImage: {}
+          }
+        },
+        xAxis: {
+          type: 'category',
+          boundaryGap: false,
+          data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            name: '邮件营销',
+            type: 'line',
+            stack: '总量',
+            data: [120, 132, 101, 134, 90, 230, 210]
+          },
+          {
+            name: '联盟广告',
+            type: 'line',
+            stack: '总量',
+            data: [220, 182, 191, 234, 290, 330, 310]
+          },
+          {
+            name: '视频广告',
+            type: 'line',
+            stack: '总量',
+            data: [150, 232, 201, 154, 190, 330, 410]
+          },
+          {
+            name: '直接访问',
+            type: 'line',
+            stack: '总量',
+            data: [320, 332, 301, 334, 390, 330, 320]
+          },
+          {
+            name: '搜索引擎',
+            type: 'line',
+            stack: '总量',
+            data: [820, 932, 901, 934, 1290, 1330, 1320]
+          }
+        ]
+      }
     };
   }
   async componentWillMount() {
@@ -131,6 +202,24 @@ export default class Home extends Component {
               </div>
             </div>
             <div className="bodyContent-body">
+              <div className="echartsBox">
+                <div className="echartsBox-bar">
+                  <DatePicker
+                    value={this.state.selectDate}
+                    placeholder="选择日期"
+                    onChange={date => {
+                      this.setState({ selectDate: date })
+                    }}
+                    // 3600 * 1000 * 24 是一天的时间
+                    disabledDate={time => time.getTime() > Date.now() - 8.64e7 + 3600 * 1000 * 24}
+                  />
+                  <div className="echartsBox-bar-blank"></div>
+                  <Button type="primary">确定</Button>
+                </div>
+                <div className="echartsBox-body">
+                  <div id="chart"></div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
