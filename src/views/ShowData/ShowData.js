6cbda81d6e2742bbbf638188452cb2d73b6f504a
unknown <1169470657@qq.com>
2018-11-16 00:03:45 +0800

    fix echart option

--- src/views/ShowData/ShowData.js
+++ src/views/ShowData/ShowData.js
@@ -17,7 +17,7 @@ import { Button, Menu, Breadcrumb, DatePicker, } from 'element-react';
 // 引入 ECharts 主模块
 import echarts from 'echarts/lib/echarts';
 // 引入折线图
-import  'echarts/lib/chart/line';
+import 'echarts/lib/chart/line';
 // 引入提示框和标题组件
 import 'echarts/lib/component/tooltip';
 import 'echarts/lib/component/title';
@@ -36,24 +36,16 @@ export default class Home extends Component {
       selectDate: new Date(),
       option: {
         title: {
-          text: '折线图堆叠'
+          text: '七天提交量'
         },
         tooltip: {
           trigger: 'axis'
-        },
-        legend: {
-          data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
         },
         grid: {
           left: '3%',
           right: '4%',
           bottom: '3%',
           containLabel: true
-        },
-        toolbox: {
-          feature: {
-            saveAsImage: {}
-          }
         },
         xAxis: {
           type: 'category',
@@ -63,38 +55,25 @@ export default class Home extends Component {
         yAxis: {
           type: 'value'
         },
-        series: [
-          {
-            name: '邮件营销',
-            type: 'line',
-            stack: '总量',
-            data: [120, 132, 101, 134, 90, 230, 210]
+        series: {
+          name: '提交总量',
+          type: 'line',
+          stack: '总量',
+          symbol: 'roundRect',//节点性状
+          itemStyle: {
+            normal: {
+              color: "#278BDD" //图标颜色
+            }
           },
-          {
-            name: '联盟广告',
-            type: 'line',
-            stack: '总量',
-            data: [220, 182, 191, 234, 290, 330, 310]
+          lineStyle: {
+            normal: {
+              width: 2,  //连线粗细
+              color: "#278BDD"  //连线颜色
+            }
           },
-          {
-            name: '视频广告',
-            type: 'line',
-            stack: '总量',
-            data: [150, 232, 201, 154, 190, 330, 410]
-          },
-          {
-            name: '直接访问',
-            type: 'line',
-            stack: '总量',
-            data: [320, 332, 301, 334, 390, 330, 320]
-          },
-          {
-            name: '搜索引擎',
-            type: 'line',
-            stack: '总量',
-            data: [820, 932, 901, 934, 1290, 1330, 1320]
-          }
-        ]
+          smooth: true,//折线图是趋缓的
+          data: [820, 932, 901, 934, 1290, 1330, 1320]
+        }
       }
     };
   }
